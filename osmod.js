adac718ccf572690c31b4aff4d9a655c288d96b9
prashant <prashant.sharma@appinventiv.com>
2023-07-05 06:41:04 +0530

    totalmemory

--- osmod.js
+++ osmod.js
@@ -9,11 +9,8 @@ console.log(`${freeMemory/1024/1024/1024}`);
 
 const totalMemory=os.totalmem();
 console.log(`${totalMemory/1024/1024/1024}`);
+const totalMemory=os.totalmem();
+console.log(`${totalMemory/1024/1024/1024}`);
+const totalMemory=os.totalmem();
+console.log(`${totalMemory/1024/1024/1024}`);
 
-console.log(os.hostname())
-
-console.log(os.platform())
-
-console.log(os.tmpdir());
-
-console.log(os.type())
